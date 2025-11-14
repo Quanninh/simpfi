a429961ff1d7251bc1558b523dd96965678ca2c1
khanh-teo1604 <10423056@student.vgu.edu.vn>
2025-11-14 14:27:55 +0100

    Add some class that can help to modify the 3 panel

---
+++ src/main/java/com/simpfi/config/Config.java
@@ -0,0 +1,10 @@
+package com.simpfi.config;
+
+public class Config {
+	// We want to define all the option command here
+
+	// For example the command of BUTTON
+	public static final String BUTTON1 = "Button 1";
+	public static final String BUTTON2 = "Button 2";
+	public static final String BUTTON3 = "Button 3";
+}
--- src/main/java/com/simpfi/ui/Frame.java
+++ src/main/java/com/simpfi/ui/Frame.java
@@ -3,9 +3,16 @@ package com.simpfi.ui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.ObjectInputFilter.Config;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Frame extends JFrame {
@@ -13,7 +20,6 @@ public class Frame extends JFrame {
 	public Frame() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1120, 660);
-		this.setVisible(true);
 		this.setTitle("Simpfi - Traffic Simulation");
 		this.setLayout(new BorderLayout(10, 10));
 
@@ -23,7 +29,10 @@ public class Frame extends JFrame {
 
 		JPanel panelTop = new JPanel();
 		JPanel panelRight = new JPanel();
-		JPanel panelCenter = new JPanel();
+		JPanel panelCenter = new DrawingPanel();
+
+		// Here we use FlowLayout for panel top
+		panelTop.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
 
 		panelTop.setBackground(Color.RED);
 		panelRight.setBackground(Color.GREEN);
@@ -36,6 +45,55 @@ public class Frame extends JFrame {
 		this.add(panelTop, BorderLayout.NORTH);
 		this.add(panelRight, BorderLayout.EAST);
 		this.add(panelCenter, BorderLayout.CENTER);
+
+		// Add new button in panelTop
+		JButton btn1 = new JButton("Btn 1");
+		btn1.addActionListener(evt -> Test_Button(evt));
+		btn1.setActionCommand(com.simpfi.config.Config.BUTTON1);
+		panelTop.add(btn1);
+		JButton btn2 = new JButton("Btn 2");
+		btn2.addActionListener(evt -> Test_Button(evt));
+		btn2.setActionCommand(com.simpfi.config.Config.BUTTON2);
+		panelTop.add(btn2);
+		JButton btn3 = new JButton("Btn 3");
+		btn3.addActionListener(evt -> Test_Button(evt));
+		btn3.setActionCommand(com.simpfi.config.Config.BUTTON3);
+		panelTop.add(btn3);
+
+		// Show the information in panelRight
+		JLabel text1 = new JLabel("This is a text");
+		panelRight.add(text1);
+
+		setVisible(true);
 	}
 
+	public void Test_Button(ActionEvent evt) {
+		System.out.println(evt.getActionCommand());
+	}
+
+	public void ShowInformation(ActionEvent evt) {
+
+	}
+
+//	public void paint(Graphics g)
+//	{
+//		Graphics2D g2D = (Graphics2D) g;
+//		g2D.setColor(Color.BLACK);
+//        g2D.drawLine(10, 10, 80, 80);
+//        g2D.drawRect(20, 20, 60, 40);
+//		this.setVisible(true);
+//	}
+
+	// Define your custom panel
+	class DrawingPanel extends JPanel {
+		@Override
+		public void paint(Graphics g) {
+			super.paintComponent(g); // clears background
+			Graphics2D g2D = (Graphics2D) g;
+
+			g2D.setColor(Color.BLACK);
+			g2D.drawLine(10, 10, 80, 80);
+			g2D.drawRect(20, 20, 60, 40);
+		}
+	}
 }
